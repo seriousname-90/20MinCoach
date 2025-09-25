977358f866d8698df02d4df4bb43646138051c1c
Ian Porras Calvo <ian.porrasc1710@gmail.com>
2025-09-25 15:50:23 -0600

    feat: Expo funcionando (aliases, tsconfig bundler, babel, ESLint)

---
+++ 20mincoach-rn/babel.config.js
@@ -0,0 +1,20 @@
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: [
+      ['module-resolver', {
+        root: ['./'],
+        alias: {
+          '@': './',                    // << clave
+          '@app': './app',
+          '@components': './components',
+          '@constants': './constants',
+          '@hooks': './hooks',
+          '@assets': './assets'
+        }
+      }],
+      'react-native-reanimated/plugin'
+    ]
+  };
+};
--- 20mincoach-rn/eslint.config.js
+++ 20mincoach-rn/eslint.config.js
@@ -1,10 +1,53 @@
-// https://docs.expo.dev/guides/using-eslint/
-const { defineConfig } = require('eslint/config');
-const expoConfig = require('eslint-config-expo/flat');
+// eslint.config.js
+import js from '@eslint/js';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import react from 'eslint-plugin-react';
+import importPlugin from 'eslint-plugin-import';
 
-module.exports = defineConfig([
-  expoConfig,
+export default [
+  // Ignora cosas que no quieres lintar
+  { ignores: ['node_modules/**', 'dist/**', '.expo/**'] },
+
+  // 🔧 Este bloque marca el config como entorno Node
   {
-    ignores: ['dist/*'],
+    files: ['eslint.config.js'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        process: 'readonly',
+        module: 'readonly',
+        require: 'readonly',
+        __dirname: 'readonly',
+      },
+    },
+    rules: {
+      'no-undef': 'off',
+    },
   },
-]);
+
+  // Tu configuración para TS/React
+  {
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        project: ['./tsconfig.json'],
+        tsconfigRootDir: process.cwd(), // <- ya no dará error
+      },
+    },
+    plugins: { '@typescript-eslint': tseslint, react, import: importPlugin },
+    settings: {
+      'import/resolver': {
+        typescript: { project: './tsconfig.json' },
+        node: { extensions: ['.ts', '.tsx', '.js', '.jsx'] },
+      },
+    },
+    rules: {
+      // ...tus reglas
+    },
+  },
+];
